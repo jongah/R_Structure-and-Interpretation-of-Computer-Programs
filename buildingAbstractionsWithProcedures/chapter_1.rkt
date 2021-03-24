d2c5ca9c61bd7d09447ae4e188a80b7fe26718ea
jongah <jongah2021@gmail.com>
2021-03-24 20:39:23 +0900

    ex_1.3 add

--- buildingAbstractionsWithProcedures/chapter_1.rkt
+++ buildingAbstractionsWithProcedures/chapter_1.rkt
@@ -25,3 +25,17 @@
 ;Exercise1.2
 (/ (+ 5 4 (- 2 (- 3 (+ 5 (/ 4 5))))) (* 3 (- 6 2) (- 2 7)))
 
+;Exercise1.3
+(define (ex_1.3_sq a1 a2)(+ (* a1 a1) (* a2 a2)))
+(define (>= x y)(not(< x y)))
+;(define (ex_1.3 x1 x2 x3)(cond ((and (>= x1 x2)(>= x2 x3)) (ex_1.3_sq x1 x2))
+;                               ((and (>= x2 x3)(>= x3 x1)) (ex_1.3_sq x2 x3))
+;                               (else (ex_1.3_sq x3 x1))))
+(define (ex_1.3 x1 x2 x3)(if (>= x1 x2)
+                             (if (>= x2 x3)
+                                 (ex_1.3_sq x1 x2)
+                                 (ex_1.3_sq x1 x3))
+                             (ex_1.3_sq x2 x3)))
+(ex_1.3 3 1 3) ; 18
+(ex_1.3 4 5 4) ; 41
+(ex_1.3 2 2 1) ; 8
